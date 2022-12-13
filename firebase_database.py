9bd8ec3afba24fb2d6e7c5c078c9c5847688758d
GreasyPepper16 <99839126+GreasyPepper16@users.noreply.github.com>
2022-12-13 11:23:41 +0100

    Update firebase_database.py

--- firebase_database.py
+++ firebase_database.py
@@ -1,6 +1,35 @@
 import firebase_admin
 from firebase_admin import credentials, db
 
+
+# ----------------------------------------------------------------------------------------- code block for pushing data
+
+def addscore(name, amount):
+    # input for testing, keeping in code for maybe later uses or in case of
+    # name, amount = input("name, amount: ").split()
+    # conversion of datatypes to be secure / for safety
+    name = str(name)
+    amount = int(amount)
+    # adding values to the object used for input
+    values = {
+        "name": name, "amount": amount
+    }
+    # pushing object to database, random object ID given by firebase
+    reference.push(values)
+
+
+# -------------------------------------------------------------------------------- code block for getting data / values
+def gettingscores():
+    # getting 10 'last' values, ordening happens in ascending order!
+    # return object is ordered dictionary
+    # results = reference.order_by_child("amount").limit_to_last(10).get()
+    # printing results dictionary mostly for test purpose of correct data retrievement
+    # keeping in code for maybe later uses or in case of
+    # print(results)
+    # returning result object, possible ordered dictionary
+    return reference.order_by_child("amount").limit_to_last(10).get()
+
+
 # ------------------------------------------------------------------ code block for setting up reference and connection
 # setting up credentials with json file
 cred = credentials.Certificate("tetris-project-python-firebase-adminsdk-y0naf-04146607aa.json")
@@ -9,20 +38,3 @@ app = firebase_admin.initialize_app(cred, {
     "databaseURL": "https://tetris-project-python-default-rtdb.europe-west1.firebasedatabase.app"})
 # getting reference of database using root collection
 reference = db.reference()
-# ----------------------------------------------------------------------------------------- code block for pushing data
-name, amount = input("name, amount: ").split()
-# conversion of datatypes to be secure / for safety
-name = str(name)
-amount = int(amount)
-# adding values to the object used for input
-values = {
-    "name": name, "amount": amount
-}
-# pushing object to database, random object ID given by firebase
-reference.push(values)
-# -------------------------------------------------------------------------------- code block for getting data / values
-# getting 10 'last' values, ordening happens in ascending order!
-# return object is ordered dictionary
-results = reference.order_by_child("amount").limit_to_last(10).get()
-# printing results dictionary mostly for test purpose of correct data retrievement
-print(results)
